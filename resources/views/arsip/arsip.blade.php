39bc5bbbab104b5020895f2ac80c8f2cb9b9b93f
Vertzh <65772692+Vertzh@users.noreply.github.com>
2021-08-29 22:10:00 +0700

    Update arsip.blade.php

--- resources/views/arsip/arsip.blade.php
+++ resources/views/arsip/arsip.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Masuk pak eko') }}
                 </div>
             </div>
         </div>
